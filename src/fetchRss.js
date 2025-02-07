30043fe380e0840a390af328b7032295e84fbbcf
Anton <atx157@gmail.com>
2025-02-07 19:23:40 +0600

    now fetchRSS provides JSON file instead XML to match playwright requirements

--- src/fetchRss.js
+++ src/fetchRss.js
@@ -2,13 +2,26 @@ import axios from 'axios';
 
 export const fetchRss = async (url) => {
   const proxyUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
+  
   try {
     const response = await axios.get(proxyUrl);
-    if (!response.data.contents) {
+
+    if (response.status !== 200) {
       throw new Error('rssLoadError');
     }
-    return response.data.contents;
+
+    const { contents } = response.data;
+
+    if (!contents || contents.trim() === '') {
+      throw new Error('rssLoadError');
+    }
+
+    return contents;
   } catch (error) {
+    if (error.response) {
+      throw new Error('rssLoadError');
+    }
     throw new Error('networkError');
   }
 };
+
